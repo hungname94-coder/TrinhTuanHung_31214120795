9ea0c537a507585e501e6c59c67c870e4819310c
Tuan Hung <hungname94@gmail.com>
2026-01-15 09:57:08 +0700

    bai tap

--- src/Lec5/Loop/bai6.java
+++ src/Lec5/Loop/bai6.java
@@ -11,21 +11,19 @@ public class bai6 {
         int a = random.nextInt(100) + 1;
         System.out.print("nhap so nguyen: ");
         int n = scanner.nextInt();
-        while (n<=1 || n>=100){
+        while (n < 1 || n > 100) {
             System.out.print("nhap lai 1-100: ");
-            n=scanner.nextInt();
+            n = scanner.nextInt();
         }
-        if (n >= 1 && n <= 100) {
-            while (n != a) {
-                if (n < a) {
-                    System.out.println("nhap so nguyen lon hon: ");
-                    n = scanner.nextInt();
-                } else if (n > a) {
-                    System.out.println("nhap so nguyen be hon: ");
-                    n = scanner.nextInt();
-                }
+        while (n != a) {
+            if (n < a) {
+                System.out.println("nhap so nguyen lon hon: ");
+                n = scanner.nextInt();
+            } else if (n > a) {
+                System.out.println("nhap so nguyen be hon: ");
+                n = scanner.nextInt();
             }
-            System.out.print("thanh cong!");
         }
+        System.out.print("thanh cong!");
     }
 }
